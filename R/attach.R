d1110351f3572744e3bd011112d7918ac4fe6204
David Kahle <david.kahle@gmail.com>
2016-06-17 01:58:54 -0500

    clean loading; interact.R -> m2.R

--- R/attach.R
+++ R/attach.R
@@ -1,5 +1,4 @@
 .onAttach <- function(...) {
-	.g_m2con <<- NULL
 
 	# find M2 on a Mac or Linux
 	if(is.mac() || is.linux()){
@@ -26,14 +25,23 @@
 	# check that the programs were found
 	startup_check_for_program("m2_path")
 
+	# initialize connection - change this?
+	.m2con <<- NULL
+
 	# return
 	invisible(TRUE)
 }
 
+
+
+
 .onDetach <- function(...) {
-	# TODO: kill M2
-	m2_end_interact()
+	stop_m2()
 }
+
+
+
+
 
 # unix_find looks for a specific executable in a specific directory
 # (or its children)
@@ -82,62 +90,64 @@ startup_check_for_program <- function(optionName){
 }
 
 longName <- function(optionName){
-	switch(optionName,
-		   m2_path = "M2"
-	)
+  switch(optionName,
+    m2_path = "M2"
+  )
 }
 
 execName <- function(optionName){
-	switch(optionName,
-		   m2_path = "count"
-	)
+  switch(optionName,
+    m2_path = "count"
+  )
 }
 
 setFun <- function(optionName){
-	switch(optionName,
-		   m2_path = "set_m2_path()"
-	)
+  switch(optionName,
+    m2_path = "set_m2_path()"
+  )
 }
 
 psm  <- packageStartupMessage
 psms <- function(fmt, ...) packageStartupMessage(sprintf(fmt, ...))
 
+
 setOption <- function(optionName, value){
-	eval(parse(text = sprintf('options("%s" = "%s")', optionName, value)))
+  eval(parse(text = sprintf('options("%s" = "%s")', optionName, value)))
 }
 
+
 unix_search_and_set <- function(exec, baseName, optionName){
-	# grab path and parse
-	profile_to_look_for <-
-		if(file.exists("~/.bash_profile")){
-			".bash_profile"
-		} else if(file.exists("~/.bashrc")){
-			".bashrc"
-		} else if(file.exists("~/.profile")){
-			".profile"
-		}
+  # grab path and parse
+  profile_to_look_for <-
+  	if(file.exists("~/.bash_profile")){
+      ".bash_profile"
+  	} else if(file.exists("~/.bashrc")){
+      ".bashrc"
+  	} else if(file.exists("~/.profile")){
+      ".profile"
+  	}
 
-	# PATH <- system(sprintf("source ~/%s; echo $PATH", profile_to_look_for), intern = TRUE)
-	# the above doesn't work on ubuntu, which uses the dash shell (which doesn't have source)
-	PATH <- system(sprintf("echo 'source ~/%s; echo $PATH' | /bin/bash", profile_to_look_for), intern = TRUE)
-	dirs_to_check <- stringr::str_split(PATH, ":")[[1]]
+  # PATH <- system(sprintf("source ~/%s; echo $PATH", profile_to_look_for), intern = TRUE)
+  # the above doesn't work on ubuntu, which uses the dash shell (which doesn't have source)
+  PATH <- system(sprintf("echo 'source ~/%s; echo $PATH' | /bin/bash", profile_to_look_for), intern = TRUE)
+  dirs_to_check <- stringr::str_split(PATH, ":")[[1]]
 
-	# check for main dir name
-	ndx_with_baseName_dir  <- which(stringr::str_detect(tolower(dirs_to_check), baseName))
-	baseName_path <- dirs_to_check[ndx_with_baseName_dir]
+  # check for main dir name
+  ndx_with_baseName_dir  <- which(stringr::str_detect(tolower(dirs_to_check), baseName))
+  baseName_path <- dirs_to_check[ndx_with_baseName_dir]
 
-	# seek and find
-	for(path in dirs_to_check){
-		found_path <- unix_find(exec, path)
-		if(!is.na(found_path)) break
-	}
+  # seek and find
+  for(path in dirs_to_check){
+    found_path <- unix_find(exec, path)
+    if(!is.na(found_path)) break
+  }
 
-	# break in a failure
-	if(is.na(found_path)) return()
+  # break in a failure
+  if(is.na(found_path)) return()
 
-	# set option and exit
-	setOption(optionName, dirname(found_path))
+  # set option and exit
+  setOption(optionName, dirname(found_path))
 
-	# invisibly return path
-	invisible(dirname(found_path))
+  # invisibly return path
+  invisible(dirname(found_path))
 }
---
+++ R/m2.R
@@ -0,0 +1,136 @@
+#' Initialize, call, and stop a Macaulay2 process
+#'
+#' Initialize, call, and stop a Macaulay2 process
+#'
+#' @param port port for Macaulay2 socket
+#' @param code Macaulay2 code
+#' @return m2 return value
+#' @name m2_init
+#' @examples
+#'
+#' start_m2()
+#'
+#' m2("1 + 1")
+#'
+#' m2("factor 32004")
+#'
+#' # compute a grobner basis of a polynomial ideal
+#' (gb <- m2("
+#'   R = QQ[a..d]
+#'   I = ideal(a^3-b^2*c, b*c^2-c*d^2, c^3)
+#'   G = gens gb I
+#' "))
+#'
+#' # parse it into an mpoly object
+#' library(stringr)
+#' library(mpoly)
+#' gb <- str_sub(gb, 10, -3)
+#' gb <- str_replace_all(gb, "\\*", " ")
+#' mp(str_split(gb, ", ")[[1]])
+#'
+#' stop_m2()
+#'
+#'
+
+
+
+
+
+m2_listen_code <- function (port) {
+  sprintf("
+    m2rint_inout = openInOut \"$:%d\";
+    while true do (
+      m2rint_inline = read m2rint_inout;
+      if m2rint_inline == \"\" then break;
+      m2rint_outline = toString value(m2rint_inline);
+      m2rint_inout << m2rint_outline << \"\\n\" << flush;
+    );
+    close m2rint_inout;
+  ", port)
+}
+# cat(m2_listen_code(6666L))
+
+
+
+
+
+
+
+
+
+#' @export
+#' @rdname m2_init
+start_m2 <- function(port = 27436L) {
+
+  # stop if no connection
+  if (!is.null(.m2con)) return(invisible(1))
+
+  # prep for m2 server process
+  if(is.mac() || is.unix()) {
+
+    system2(
+      file.path2(getOption("m2_path"), "M2"),
+      stdout = NULL, stderr = NULL,
+      stdin = write_to_temp(m2_listen_code(port)),
+      wait = FALSE
+    )
+    Sys.sleep(1)
+
+  }  else if(is.win()) {
+
+    # TODO: fix later
+    # matFile <- file.path2(dir2, "countCode.latte")
+    # matFile <- chartr("\\", "/", matFile)
+    # matFile <- str_c("/cygdrive/c", str_sub(matFile, 3))
+    # system2(
+    #   "cmd.exe",
+    #   paste(
+    #     "/c env.exe",
+    #     file.path(getOption("latte_path"), "count"),
+    #     opts, matFile
+    #   ),
+    #   stdout = "countOut", stderr = "countErr"
+    # )
+
+  }
+
+  # initialize client socket
+  .m2con <<- socketConnection(
+  	host = "localhost", port = port,
+  	blocking = TRUE, server = FALSE,
+  	open = "r+", timeout = 60*60*24*7
+  )
+
+}
+
+
+
+
+
+#' @export
+#' @rdname m2_init
+stop_m2 <- function() {
+  if (!is.null(.m2con)) {
+    writeLines("", .m2con)
+    close(.m2con)
+    .m2con <<- NULL
+  }
+}
+
+
+
+
+#' @export
+#' @rdname m2_init
+m2 <- function(code) {
+
+  # preempt m2 kill code
+  if (code == "") return("")
+
+  # write to connection
+  writeLines(code, .m2con)
+
+  # read from connection and return
+  readLines(.m2con, 1)
+
+}
