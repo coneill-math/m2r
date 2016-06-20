f51d32ac997fd44233955aa49b824b2f2e79bd9d
Christopher ONeill <musicman3320@gmail.com>
2016-06-19 23:05:05 -0700

    added rm_m2() for garbage collection fixed #21

---
+++ R/rm.R
@@ -0,0 +1,31 @@
+#' Clears a Macaulay2 variable
+#'
+#' Clears a Macaulay2 variable
+#'
+#' @param x the M2 object to clear
+#' @return Invisible the object passed in.
+#' @export
+#' @examples
+#'
+#' \dontrun{ requires Macaulay2 be installed
+#'
+#' R <- ring(c("x1", "x2", "x3"))
+#' m2(paste("class", R$m2name))
+#'
+#' rm_m2(R)
+#'
+#' m2(paste("class", R$m2name))
+#'
+#' }
+#'
+
+rm_m2 <- function (x) {
+
+  if (!is.m2(x)) {
+    return(invisible())
+  }
+
+  m2(paste(x$m2name, "=symbol", x$m2name))
+
+  invisible()
+}
