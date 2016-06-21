680e75a1936eec3feb7c5f79c37b25d0eae4aa19
Jeff Sommars <sommars1@uic.edu>
2016-06-20 21:59:13 -0500

    solve_system implemented

--- R/PHCpack.R
+++ R/PHCpack.R
@@ -1,9 +1,37 @@
-#' for this to work, you need to have modified your init-PHCpack.m2 file
+#' @examples
+#'
+#' Note that this doesn't take in an input ring
+#' because this solver only works over the complex numbers
+#'
+#' \dontrun{ requires Macaulay2 be installed
+#'
+#' For this to work, you need to have modified your init-PHCpack.m2 file
 #' instead of changing your .bashrc file to establish the path of phc
+#'
+#' (mpolyList <- mp(c("t^4 - x", "t^3 - y", "t^2 - z", "x+y+z")))
+#' solve_system(mpolyList)
+#' }
 
-solve_system <- function (mpolyList, ring) {
-  m2("needsPackage \"PHCpack\"")
-  m2("R = CC[x,y]")
-  m2("a = solveSystem {x+y^2-4*x*y,x^3+5*x*y+y}")
-  m2("for i in 0..(#a-1) list (a#i#Coordinates)")
+solve_system <- function (mpolyList) {
+
+  # allow for character vectors
+  if(is.character(mpolyList)) mpolyList <- mp(mpolyList)
+
+  # convert mpolylist to strings readable by m2
+  poly_str <- suppressMessages(paste0( lapply(mpolyList, print, stars=TRUE), collapse=", "))
+  poly_str <- str_replace_all(poly_str, "\\*\\*", "^")
+
+  var_str <- suppressMessages(paste0(vars(mpolyList),collapse=","))
+  # TODO: rename
+  m2_code <- sprintf(
+    "needsPackage \"PHCpack\";
+    R := CC[%s];
+    pts := solveSystem {%s};
+    for i in 0..(#pts-1) list (pts#i#Coordinates)",
+    var_str,
+    poly_str
+    )
+
+  m2_out <- m2(m2_code)
+  m2_out
 }
