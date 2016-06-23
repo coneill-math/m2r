9e7cb6fe74e9951e08c19452362c310cc257f8d3
Jeff Sommars <sommars1@uic.edu>
2016-06-22 22:13:59 -0500

    Getting close to having core functionality of PHCpack implemented.

---
+++ R/PHCpack_helpers.R
@@ -0,0 +1,25 @@
+mpolyList_to_m2_str <- function(mpolyList) {
+  # allow for character vectors
+  if(is.character(mpolyList)) mpolyList <- mp(mpolyList)
+
+  # convert mpolylist to strings readable by m2
+  poly_str <- suppressMessages(paste0( lapply(mpolyList, print, stars=TRUE), collapse=", "))
+  poly_str <- str_replace_all(poly_str, "\\*\\*", "^")
+
+  poly_str
+}
+
+m2_pts_str_to_list <-function(m2_out) {
+  m2_out <- str_sub(m2_out,2,-2)
+  m2_out <- str_replace_all(m2_out, "p53", "")
+  m2_out <- str_replace_all(m2_out, "toCC\\(", "complex(real=")
+  m2_out <- str_replace_all(m2_out, ",complex", "Dcomplex")
+  m2_out <- str_replace_all(m2_out, "\\},", "\\}")
+  m2_out <- str_replace_all(m2_out, ",",",imaginary=")
+  m2_out <- str_replace_all(m2_out, "Dcomplex", ",complex")
+  m2_out <- str_replace_all(m2_out, "\\{", "c\\(")
+  m2_out <- str_replace_all(m2_out, "\\}", "\\),")
+  m2_out <- paste0("list(",str_sub(m2_out,0,-2),")")
+  m2_out <- eval(parse(text=m2_out))
+  m2_out
+}
