84e384e09e416973a76a08b80188e30e2a1539ab
Jeff Sommars <sommars1@uic.edu>
2016-07-09 12:07:27 -0500

    Horrible string parsing to convert to a factor matrix

--- R/factor_poly.R
+++ R/factor_poly.R
@@ -25,5 +25,53 @@ factor_poly <- function (mpoly, ring, code = FALSE) {
 
   if(code) message(m2_code)
   m2_out <- m2(m2_code)
-  m2_out
+  m2_out_vec <- substring(m2_out, seq(1,nchar(m2_out),1), seq(1,nchar(m2_out),1))
+
+  paren_count <- 0
+  factor_vec <- c()
+  power_vec <- c()
+  start_index <- 1
+  last_close_paren_index <- 0
+  for (i in 1:length(m2_out_vec)){
+    char <- m2_out_vec[i]
+    if (char == "(") {
+      paren_count <- paren_count + 1
+      # We have found a new factor
+      if((paren_count == 1) & (i != 1)) {
+        # i-2 because the current character is an open paren and the previous
+        # character is a *
+        factor_str <- substring(m2_out, start_index, last_close_paren_index)
+        factor_vec <- c(factor_vec, (factor_str))
+        if(m2_out_vec[last_close_paren_index + 1] == "^"){
+          power_str <- substring(m2_out,last_close_paren_index + 2,i-2)
+        } else {
+          power_str <- 1
+        }
+        power_vec <- c(power_vec, power_str)
+        start_index <- i
+      }
+    }
+    if (char == ")") {
+      paren_count <- paren_count - 1
+      last_close_paren_index <- i
+    }
+  }
+
+  # This case happens if the user inputs a polynomial without any variables.
+  if(last_close_paren_index == 0){
+    factor_vec <- c(mp(m2_out))
+    power_vec <- c(1)
+  } else {
+    # This case takes care of the last factor of the polynomial
+    factor_str <- substring(m2_out, start_index, last_close_paren_index)
+    factor_vec <- c(factor_vec, (factor_str))
+    if (nchar(m2_out) != last_close_paren_index){
+      if(m2_out_vec[last_close_paren_index + 1] == "^"){
+        power_str <- substring(m2_out,last_close_paren_index + 2,nchar(m2_out))
+    }} else {
+      power_str <- 1
+    }
+    power_vec <- c(power_vec, power_str)
+  }
+  matrix(c(factor_vec,power_vec),ncol=2)
 }
