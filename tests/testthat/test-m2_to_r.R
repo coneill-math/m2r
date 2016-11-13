3d9c411cabf083131546de9644dd7fdb8879d1e5
Jeff Sommars <sommars1@uic.edu>
2016-11-13 00:00:52 -0600

    First draft of parser tests

--- tests/testthat/test-m2_to_r.R
+++ tests/testthat/test-m2_to_r.R
@@ -13,6 +13,10 @@ test_that("m2_parse parses basic data structures",{
   m2_array_output <- structure(list(1,2,3), class = c("m2_array", "m2"))
   m2_parse_test(m2_array,m2_array_output)
 
+  m2_sequence <- "(1,2,3)"
+  m2_sequence_output <- structure(list(1,2,3), class = c("m2_sequence", "m2"))
+  m2_parse_test(m2_sequence, m2_sequence_output)
+
   m2_str <- "\"my string\""
   m2_str_output <- "my string"
   m2_parse_test(m2_str, m2_str_output)
@@ -27,17 +31,16 @@ test_that("m2_parse parses basic data structures",{
 
   # toExternalString(matrix{{1,2,3},{4,5,6}})
   m2_matrix <- "map((ZZ)^2,(ZZ)^3,{{1, 2, 3}, {4, 5, 6}})"
-  m2_matrix_output <- " "
-  #str(m2_parse(m2_matrix))
-  #m2_parse_test(m2_matrix,m2_matrix_output)
+  m2_matrix_output <- structure(list(m2_name = NULL, rmatrix = matrix(c(1,4,2,5,3,6),nrow = 2, ncol = 3)), class = c("m2_matrix", "m2"))
+  m2_parse_test(m2_matrix,m2_matrix_output)
+
+  # toExternalString(matrix{{}})
+  m2_empty_matrix <- "map((ZZ)^1,(ZZ)^0,0)"
+  m2_empty_matrix_output <- structure(list(m2_name = NULL, rmatrix = matrix(numeric(0), nrow = 0, ncol = 0)), class = c("m2_matrix", "m2"))
+  m2_parse_test(m2_empty_matrix,m2_empty_matrix_output)
 })
 
-test_that("m2 parses compound data structures", {
-  # toExternalString(CC[x,y])
-  m2_ring <- "CC_53(monoid[x..y, Degrees => {2:1}, Heft => {1}, MonomialOrder => VerticalList{MonomialSize => 32, GRevLex => {2:1}, Position => Up}, DegreeRank => 1])"
-  m2_ring_output <- " "
-  #m2_parse_test(m2_ring, m2_ring_output)
-
+test_that("m2 parses harder data structures", {
   m2_poly_list <- "{b*c*e-a*d*f, a*c*e-b*d*f}"
   m2_poly_list_output <- structure(list(structure("b*c*e-a*d*f", class = c("m2_expression", "m2")),structure("a*c*e-b*d*f", class = c("m2_expression", "m2"))),class = c("m2_list", "m2"))
   m2_parse_test(m2_poly_list, m2_poly_list_output)
@@ -45,14 +48,20 @@ test_that("m2 parses compound data structures", {
   # toExternalString(factor(2^4*3^3))
   m2_factor <- "new Product from {new Power from {2,4},new Power from {3,3}}"
   m2_factor_output <- structure(list(structure(list(2,4),class = c("m2_power" , "m2")),structure(list(3,3),class = c("m2_power" , "m2"))), class = c("m2_product", "m2"))
-  #m2_parse_test(m2_factor, m2_factor_output)
-
-  # toExternalString(ideal(a*b*c-d*e*f,a*c*e-b*d*f,a*d*f-b*c*e))
-  m2_ideal <- "ideal map((R)^1,(R)^{{-3},{-3},{-3}},{{a*b*c-d*e*f, a*c*e-b*d*f, -b*c*e+a*d*f}})"
-  m2_ideal_output <- " "
-  #m2_parse_test(m2_ideal, m2_ideal_output)
+  m2_parse_test(m2_factor, m2_factor_output)
 
   m2_compound_test <- "new OptionTable from {Headline => \"test\", Configuration => new OptionTable from {}}"
   m2_compound_test_output <- structure(list(structure(list(structure("Headline",class=c("m2_symbol", "m2")),"test"),class = c("m2_option","m2")), structure(list(structure("Configuration",class = c("m2_symbol", "m2")),structure(list(),class=c("m2_optiontable","m2"))), class = c("m2_option", "m2"))), class = c("m2_optiontable", "m2"))
   m2_parse_test(m2_compound_test, m2_compound_test_output)
+
+  # toExternalString(ideal(a*b*c-d*e*f,a*c*e-b*d*f,a*d*f-b*c*e))
+  # This test cheats and uses QQ to work instead of actually using some anonymous ring it should
+  m2_ideal <- "map((QQ)^1,(QQ)^{1},{{a*b*c-d*e*f, a*c*e-b*d*f, -b*c*e+a*d*f}})"
+  m2_ideal_output <- structure(list(m2_name = NULL, rmatrix = matrix(c("a*b*c-d*e*f", "a*c*e-b*d*f", "-b*c*e+a*d*f"),nrow = 1, ncol = 3)),class = c("m2_matrix", "m2"))
+  m2_parse_test(m2_ideal, m2_ideal_output)
+
+  # toExternalString(CC[x,y])
+  m2_ring <- "CC_53(monoid[x..y, Degrees => {2:1}, Heft => {1}, MonomialOrder => VerticalList{MonomialSize => 32, GRevLex => {2:1}, Position => Up},DegreeRank => 1])"
+  m2_ring_output <- structure(list(m2_name = NULL, coefring = "CC", vars = c("x", "y"), order = "grevlex"),class = c("m2_polynomialring", "m2"))
+  m2_parse_test(m2_ring, m2_ring_output)
 })
