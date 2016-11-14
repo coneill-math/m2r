e52ce9439ec3c9bbfb796358d9554ce700176238
David Kahle <david.kahle@gmail.com>
2016-11-14 09:45:10 -0600

    update factor_poly; allow it to accept pointers; add tests (some are breaking)

--- tests/testthat/test-factor_poly.R
+++ tests/testthat/test-factor_poly.R
@@ -1,16 +1,99 @@
 context("factor_poly")
 
+
+# define objects common to the tests
+expected_output <- list(
+  mpolyList = mp(c("x-y", "x+y", "x^2+y^2")),
+  power = c(1L, 1L, 1L)
+)
+
+QQxy <- ring(c("x","y"), "QQ")
+QQxy_pointer <- ring.(c("x","y"), "QQ")
+
+
+# run tests
+test_that("factor_poly(char, m2_polynomialring)", {
+  expect_equal(factor_poly("x^4 - y^4", QQxy), expected_output)
+})
+
 test_that("factor_poly(mpoly, m2_polynomialring)", {
+  expect_equal(factor_poly(mp("x^4 - y^4"), QQxy), expected_output)
+})
 
-  expect_equal(
-    factor_poly(
-      mp("x^4 - y^4"),
-      ring(c("x","y"), "QQ", "lex")
-    ),
-    list(
-      mpolyList = mp(c("x-y", "x+y", "x^2+y^2")),
-      power = c(1L, 1L, 1L)
-    )
-  )
+test_that("factor_poly(m2_pointer, m2_polynomialring)", {
+  expect_equal(factor_poly(m2.("x^4 - y^4"), QQxy), expected_output)
+})
 
+test_that("factor_poly(char, m2_pointer)", {
+  expect_equal(factor_poly("x^4 - y^4", QQxy_pointer), expected_output)
+})
+
+
+test_that("factor_poly(mpoly, m2_pointer)", {
+  expect_equal(factor_poly(mp("x^4 - y^4"), QQxy_pointer), expected_output)
+})
+
+test_that("factor_poly(m2_pointer, m2_pointer)", {
+  expect_equal(factor_poly(m2.("x^4 - y^4"), QQxy_pointer), expected_output)
+})
+
+
+
+
+
+
+
+
+context("factor_poly.")
+
+
+# define objects common to the tests
+expected_output <- structure(
+  list(
+    ext_str = "new Product from {new Power from {x-y,1},new Power from {x+y,1},new Power from {x^2+y^2,1}}",
+    m2_name = "m2o#",
+    m2_class = "Product",
+    m2_class_class = "WrapperType"
+  ),
+  .Names = c("ext_str", "m2_name", "m2_class", "m2_class_class"),
+  class = c("m2_pointer", "m2")
+)
+
+
+# run tests
+test_that("factor_poly.(char, m2_polynomialring)", {
+  o <- factor_poly.("x^4 - y^4", QQxy)
+  o$m2_name <- "m2o#"
+  expect_equal(o, expected_output)
+})
+
+test_that("factor_poly.(mpoly, m2_polynomialring)", {
+  o <- factor_poly.(mp("x^4 - y^4"), QQxy)
+  o$m2_name <- "m2o#"
+  expect_equal(o, expected_output)
+})
+
+test_that("factor_poly.(m2_pointer, m2_polynomialring)", {
+  o <- factor_poly.(m2.("x^4 - y^4"), QQxy)
+  o$m2_name <- "m2o#"
+  expect_equal(o, expected_output)
+})
+
+test_that("factor_poly.(char, m2_pointer)", {
+  o <- factor_poly.("x^4 - y^4", QQxy_pointer)
+  o$m2_name <- "m2o#"
+  expect_equal(o, expected_output)
+})
+
+
+test_that("factor_poly.(mpoly, m2_pointer)", {
+  o <- factor_poly.(mp("x^4 - y^4"), QQxy_pointer)
+  o$m2_name <- "m2o#"
+  expect_equal(o, expected_output)
+})
+
+test_that("factor_poly.(m2_pointer, m2_pointer)", {
+  o <- factor_poly.(m2.("x^4 - y^4"), QQxy_pointer)
+  o$m2_name <- "m2o#"
+  expect_equal(o, expected_output)
 })
