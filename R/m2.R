ed00c3d5112f5a3df985b2723af76f46884e06fe
Kahle <David_Kahle@baylor.edu>
2016-12-01 09:52:29 -0600

    Reference #24

--- R/m2.R
+++ R/m2.R
@@ -144,18 +144,46 @@ do_start_m2 <- function(port = 27436L, timeout = 10) {
     )
 
   } else if(is.win()) {
+browser()
 
+    # this works from dos command line with some complaining
+    # env.exe PATH=/usr/bin:/cygdrive/c/cygwin/lib/lapack /usr/bin/M2
+
+    # i've tried the following.... (and more)
+    # env.exe PATH=/usr/bin:$PATH:/cygdrive/c/cygwin/lib/lapack /usr/bin/M2
+    # env.exe PATH=/usr/bin:$PATH:/cygdrive/c/cygwin/lib/lapack:/usr/share/Macaulay2:/usr/share/doc/Macaulay2:/usr/share/Macaulay2/Core /usr/bin/M2
+
+    # ...but i can't stop the complaining unless i use
+    # env.exe PATH=/usr/bin:/cygdrive/c/cygwin/lib/lapack /usr/bin/M2 --no-setup
+    # note: if you run that, "exit" won't work in M2, you have to kill the process
+    # manually from the task manager
+
+    # this launches M2 with no errors... but kills it immediately
+    system("cmd /K C:\\cygwin\\bin\\env.exe PATH=/usr/bin:$PATH:/usr/share/Macaulay2/Core:/usr/share/doc/Macaulay2:/cygdrive/c/cygwin/lib/lapack /usr/bin/M2 --no-setup")
+
+    # and this launches the persistent session, but otherwise doesn't work.
+    # e.g. m2("1+1") hangs
     system2(
-      "cmd.exe",
-      paste(
-        "/K",# env.exe",
-        file.path2(get_m2_path(), "env.exe"),
-        file.path2(get_m2_path(), "M2.exe")
-      ),
+      "cmd",
+      "/K C:\\cygwin\\bin\\env.exe PATH=/cygdrive/c/cygwin/lib/lapack:/usr/bin /usr/bin/M2 --no-setup",
       stdout = NULL, stderr = NULL,
       stdin = write_to_temp(m2_listen_code(port, timeout)),
       wait = FALSE
     )
+
+    # creates a process that dies immediately
+#     system2(
+#       "cmd.exe",
+#       paste(
+#         "/K",# env.exe",
+#         file.path2(get_m2_path(), "env.exe"),
+#         "PATH=/cygdrive/c/cygwin/lib/lapack /usr/bin/M2"
+#         # file.path2(get_m2_path(), "M2.exe")
+#       ),
+#       stdout = NULL, stderr = NULL,
+#       stdin = write_to_temp(m2_listen_code(port, timeout)),
+#       wait = FALSE
+#     )
 
   }
 
