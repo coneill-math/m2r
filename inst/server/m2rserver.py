7b3cf0492baef3ae65af47bcd1c0acf0b09f4833
Christopher ONeill <musicman3320@gmail.com>
2017-05-06 16:43:52 -0500

    updated server code

--- inst/server/m2rserver.py
+++ inst/server/m2rserver.py
@@ -1,6 +1,8 @@
 import socket
 import sys
 import time
+import random
+import string
 
 from subprocess import Popen
 from subprocess import call
@@ -10,68 +12,70 @@ MAX_PORTS = 20
 FIRST_PORT = 27436
 
 
-def randomString(N = 10):
+def generateLogFileName(N = 10):
 	randpart = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(N))
 	
-	ret = "/m2_logs/session-"
+	ret = "session-"
 	ret = ret + time.strftime("%Y-%m-%d-%H-%M-%S-", time.gmtime())
-	ret = randpart + ".txt"
+	ret = ret + randpart + ".txt"
 	
 	return ret
 	
 
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as serversocket:
+	print(socket.gethostname())
+	serversocket.bind((socket.gethostname(), 27435))
+	serversocket.listen(5)
 
-print(socket.gethostname())
-serversocket.bind((socket.gethostname(), 27434))
-serversocket.listen(5)
 
-
-while True:
-	# Wait for a connection
-	print("Waiting for a connection")
-	connection, client_address = serversocket.accept()
-	
-	try:
-		print("Connection from ", client_address)
+	while True:
+		# Wait for a connection
+		print("Waiting for a connection")
+		connection, client_address = serversocket.accept()
 		
-		openport = 0
-		containername = ""
+		try:
+			print("Connection from " + str(client_address))
+			
+			openport = 0
+			containername = ""
+			
+			portrange = range(FIRST_PORT, FIRST_PORT+MAX_PORTS)
+			
+			# find the first open port
+			for port in portrange:
+				with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+					if sock.connect_ex(("localhost",port)) != 0:
+						# create M2 docker instance
+						containername = "m2rserver" + str(port)
+						retval = call(["docker", "create", "-it", "--expose=" + str(port), "--publish=" + str(port) + ":" + str(port), "--name=" + containername, "sommars/m2r"])
+						if retval == 0:
+							openport = port
+							break
+			
+			if openport != 0:
+				logfilename = generateLogFileName()
+				
+				# launch M2 docker instance
+				cmd = ["docker", "start", containername]
+				cmd = cmd + [";", "docker", "exec", containername, "M2", "--script", "/m2r_server_script.m2", str(openport)]#, "&>" + logfilename]
+				cmd = cmd + [";", "docker", "stop", containername]
+				cmd = cmd + [";", "docker", "rm", containername]
+				# cmd = cmd + [";", "aws", "s3", "cp", logfilename, "s3://my_bucket/my_folder/my_file.ext"]
+				Popen(" ".join(cmd), shell=True)
+				# p.terminate()
+				
+				print("Session logging to " + logfilename)
+			else:
+				print("No session started, all ports taken")
+			
+			# send open port back to the client
+			connection.sendall(str(openport) + "\n")
 		
-		portrange = range(FIRST_PORT, FIRST_PORT+MAX_PORTS)
+		finally:
+			# Clean up the connection
+			connection.close()
 		
-		# find the first open port
-		for port in portrange:
-			with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
-				if sock.connect_ex(("localhost",port)) != 0:
-					# create M2 docker instance
-					containername = "m2rserver" + str(openport)
-					retval = call(["docker", "create", "-it", "--expose=" + str(port), "--publish=" + str(port) + ":" + str(port), "--name=" + containername, "sommars/m2r"])
-					if retval == 0:
-						openport = port
-						break
-		
-		logfilename = logFileName()
-		
-		outredir = ["2&>1", ">" + logfilename]
-		# launch M2 docker instance
-		cmd = ["docker", "start", containername] + outredir
-		cmd = cmd + [";", "docker", "exec", containername, "M2", "--script", "/m2r_server_script.m2", str(openport)] + outredir
-		cmd = cmd + [";", "docker", "stop", containername] + outredir
-		cmd = cmd + [";", "docker", "rm", containername] + outredir
-		# cmd = cmd + [";", "aws", "s3", "cp", logfilename, "s3://my_bucket/my_folder/my_file.ext"]
-		Popen(" ".join(cmd), shell=True)
-		# p.terminate()
-		
-		print("Session logging to " + logfilename)
-		
-		# send open port back to the client
-		connection.sendall(str(openport) + "\n")
-	
-	finally:
-		# Clean up the connection
-		connection.close()
-	
+		print("")
 
 
 
